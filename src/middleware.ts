e26f29afeb6436107d0b21585ac7499032e6d246
Sanket Dadali <sanketdadali05@gmail.com>
2024-08-10 18:39:47 +0530

    zustand setup and code done

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import getOrCreateDB from "./models/server/seed";
+import getOrCreateStorage from "./models/server/storage.collection";
+
+export async function middleware(request: NextRequest) {
+  await Promise.all([getOrCreateDB(), getOrCreateStorage()]);
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
